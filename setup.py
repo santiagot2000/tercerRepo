fda87e6d381177e69f40e0fec996d30b14add249
santiago torres <santiagotorresmonsalve@gmail.com>
2025-05-16 14:07:49 -0500

    primer lanzamiento

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="tercerRepo",                                 # Nombre del paquete
+    version="0.1.0",                                   # Versión inicial
+    packages=find_packages(),                          # Paquetes a incluir
+    description="Un paquete pip simple de saludo",     # Breve descripción
+    author="santiago torres",                          # Tu nombre
+    author_email="santiagotorresmonsalve@gmail.com",   # Tu correo electrónico
+    url="https://github.com/santiagot2000/tercerRepo", # URL del proyecto
+)
